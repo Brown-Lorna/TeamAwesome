62e806722043ac4234c7b992515006568167dff6
davault <davetheriault@gmail.com>
2015-11-28 14:18:59 -0700

    Merge origin/master
    Conflicts:
    	.openshift/README.md
    	.openshift/cron/README.cron
    	.openshift/markers/README.md

--- FB/src/main/java/edu/byui/fb/JDBCClass.java
+++ FB/src/main/java/edu/byui/fb/JDBCClass.java
@@ -1,64 +1,81 @@
-
-
 //STEP 1. Import required packages
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JDBCClass {
-   // JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/";
 
-   //  Database credentials
-   static final String USER = "username";
-   static final String PASS = "password";
-   
-   public static void main(String[] args) {
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //STEP 2: Register JDBC driver
-      Class.forName("com.mysql.jdbc.Driver");
+    static String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+    static String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+    static String username = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+    static String password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:myql://" + host + ":" + port + "/ancestor";
 
-      //STEP 3: Open a connection
-      System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+    //  Database credentials
+    static final String USER = username;
+    static final String PASS = password;
 
-      //STEP 4: Execute a query
-      System.out.println("Creating database...");
-      stmt = conn.createStatement();
-      
-      String sql = "CREATE DATABASE STUDENTS";
-      stmt.executeUpdate(sql);
-      System.out.println("Database created successfully...");
-   }catch(SQLException se){
-      //Handle errors for JDBC
-      se.printStackTrace();
-   }catch(Exception e){
-      //Handle errors for Class.forName
-      e.printStackTrace();
-   }finally{
-      //finally block used to close resources
-      try{
-         if(stmt!=null)
-            stmt.close();
-      }catch(SQLException se2){
-      }// nothing we can do
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }//end finally try
-   }//end try
-   System.out.println("Goodbye!");
-   
-}//end main
-   
-    private static void getPeople(){
-        
-        
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating database...");
+            stmt = conn.createStatement();
+
+            String sql = "CREATE DATABASE STUDENTS";
+            stmt.executeUpdate(sql);
+            System.out.println("Database created successfully...");
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+
+    }//end main
+
+    private void getPeople() {
+
+        try {
+            //connect
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = null;
+            String sql = "SELECT * FROM person;";
+            ResultSet rs = stmt.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    private static void getParents(String child) {
+
+    private void getParents(String child) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }//end JDBCExample
